9686dc01d64284f40a80a7aeeb848fbba76dea01
Bao Duong <bao.duong@VNLT057.local>
2021-10-21 16:37:31 +0700

    Added lib angular firebase and tools

--- funnyvideo/src/environments/environment.ts
+++ funnyvideo/src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'funny-video-578ac',
+    appId: '1:999545879604:web:dbc5cc5bbe8edd032e1e61',
+    storageBucket: 'funny-video-578ac.appspot.com',
+    apiKey: 'AIzaSyCElbdLNw7kgCImaKBlvT9DrvOmjr7UdhI',
+    authDomain: 'funny-video-578ac.firebaseapp.com',
+    messagingSenderId: '999545879604',
+    measurementId: 'G-Y86JQ3E90P',
+  },
   production: false
 };
 
